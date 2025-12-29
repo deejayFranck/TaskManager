66020c6cdc7e65d10df48852124eb2013fe3a505
Franck Djatcha <djatchafranck@gmail.com>
2025-12-29 17:35:54 +0100

    Data layer CRUD operaations tested

---
+++ src/main/java/com/linkedin/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.linkedin.taskmanager.repository;
+
+import com.linkedin.taskmanager.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface TaskRepository  extends JpaRepository<Task, Long> {
+
+}
---
+++ src/test/java/com/linkedin/taskmanager/repository/TaskRepositoryTest.java
@@ -0,0 +1,97 @@
+package com.linkedin.taskmanager.repository;
+
+import com.linkedin.taskmanager.model.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class TaskRepositoryTest {
+
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Test
+    void testSaveTask(){
+        // arrange
+        Task task = new Task("Test task", "To do");
+
+        // act
+        Task savedTask = taskRepository.save(task);
+
+        // assert
+        assertNotNull(savedTask);
+        assertEquals("Test task", savedTask.getTitle());
+    }
+
+    @Test
+    void testFindAllTasks(){
+        // arrange
+        Task task1 = new Task("Task 1", "To do");
+        taskRepository.save(task1);
+
+        Task task2 = new Task("Task 2", "Done");
+        taskRepository.save(task2);
+
+        // act
+        List<Task> tasks = taskRepository.findAll();
+
+        // assert
+        assertEquals(2, tasks.size());
+    }
+
+    @Test
+    void testFindTaskById(){
+        // arrange
+        Task task = new Task("Test task", "To do");
+        taskRepository.save(task);
+
+        // act
+        task.setStatus("Done");
+        taskRepository.save(task);
+        Optional<Task> optionalTask = taskRepository.findById(task.getId());
+
+
+        // assert
+        assertFalse(optionalTask.isEmpty());
+        assertEquals("Done", optionalTask.get().getStatus());
+    }
+
+
+    @Test
+    void testUpdateTask(){
+        // arrange
+        Task task = new Task("Test task", "To do");
+        taskRepository.save(task);
+
+        // act
+        Optional<Task> optionalTask = taskRepository.findById(task.getId());
+
+
+        // assert
+        assertFalse(optionalTask.isEmpty());
+        assertEquals(task.getId(), optionalTask.get().getId());
+    }
+
+
+    @Test
+    void testDeleteTask(){
+
+        // arrange
+        Task task = new Task("Task to delete", "Done");
+        taskRepository.save(task);
+
+        // act
+        taskRepository.delete(task);
+        Optional<Task> deletedTask = taskRepository.findById(task.getId());
+
+        // assert
+        assertFalse(deletedTask.isPresent());
+    }
+}
